e58433a93e0d743685549c4d84c3a72771c7bbc5
araymer <aaronraymer26@gmail.com>
2015-10-13 08:51:30 -0700

    Minor adjustments
    -Fixed main logo sizing and positioning (was too small and off-center)
    -Fixed Safari nab-bar issue with positioning (added load method to set
    it manually after document loaded)
    -Fixed main div issue

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@ $(function() {
 	var  mn = $(".slide-header");
 		mns = "main-nav-scrolled";
 		hdr = $('#top-full-splash').height()+50;
-
+	//This controls the sticky behavior of the navbar and toggles the red bit with the logos above it.
 	$(window).scroll(function() {
 	  if( $(this).scrollTop() > hdr ) {
 		mn.addClass(mns);
@@ -13,11 +13,16 @@ $(function() {
 		$('#slide-images').css('display','none');
 	  }
 	});
+	//This sets our nav bar to the bottom of our image, because it scales responsively with the width. 
+	$(window).load(function() {
+		hdr = $('#top-full-splash').height()+50;
+		$('#slide-wrapper').css("top", $('#top-full-splash').height() + 150);
+
 	
-	$('#slide-wrapper').css("top", $('#top-full-splash').height() + 150);
-	
-	$(window).on('resize', function() {
+	});
 		
+		//This makes sure that if the size changes on-the-fly that the navbar sticks to the bottom of our senate splash picture.
+	$(window).on('resize', function() {
 		hdr = $('#top-full-splash').height() + 100;
 		$('#slide-wrapper').css("top", $('#top-full-splash').height() + 150);
 		
