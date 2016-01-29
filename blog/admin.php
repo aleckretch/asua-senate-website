bc43e2387031ca46db5d0831cd0810a37d4b4d22
Dilan Jenkins <djenkins1@email.arizona.edu>
2016-01-28 18:01:06 -0700

    Backend integrated with server

--- blog/admin.php
+++ blog/admin.php
@@ -18,7 +18,7 @@ function showMessage( $str , $correctStr, $messageCorrect, $messageWrong )
 	}
 }
 
-function checkUploaded( $isUpload, $notUploadStr, $str, $correctStr, $messageCorrect, $messageWrong )
+function checkUploaded( $isUpload, $notUploadStr, $str, $correctStr, $messageCorrect, $messageWrong, $sanitize = TRUE )
 {
 	if ( $isUpload )
 	{
@@ -26,7 +26,14 @@ function checkUploaded( $isUpload, $notUploadStr, $str, $correctStr, $messageCor
 	}
 	else
 	{
-		echo Database::sanitizeData( $notUploadStr );
+		if ( $sanitize )
+		{
+			echo Database::sanitizeData( $notUploadStr );
+		}
+		else
+		{
+			echo $notUploadStr;
+		}
 	}
 }
 
@@ -37,16 +44,83 @@ if ( !Session::userLoggedIn() )
 	exit();
 }
 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>University of Arizona</title>
+	<script src="tinymce_4.3.3/tinymce/js/tinymce/tinymce.min.js"></script>
+	<style>
+	h1{text-align: center;color: #536DFE;}
+	h2{color: #536DFE;}
+	footer{text-align: center;}
+	body {background-color:lightgrey;}
+	input{margin: 5px;}
+	FORM { border : solid gray 2px; padding : 3px;}
+	</style>
+</head>
+<body>
+      <header><!-- Start header -->
+         <h1 id="logo">ASUA Upload Archive</h1>
+      </header><!-- End header -->
+      <a href="../index.html">Return to Page</a><br/>
+      <a href="admin.php?blog=yes">Post Blog Entry</a><br/>
+      <a href="admin.php?agenda=yes">Upload Agenda</a><br/>
+
+    <div id="Submit"><!-- Start content -->
+<?php
+
 $req = $_GET;
 $uploaded = isset( $req[ 'uploaded' ] );
 $correct = ( $uploaded ? $req[ 'uploaded' ] : "" );
+$token = Session::token();
 if ( isset( $req[ 'blog' ] ) )
 {
-	checkUploaded( $uploaded, "TODO: Blog form needed" , $correct , "yes" , "Blog post created" , $correct );
+	$str = "
+	   <form action='blogUpload.php' method='post'>
+	   <div>
+	   <h2>Blog Post Entry</h2>
+	   <form action='index.php' method='post'>
+			<input type='hidden' name='token' value='${token}'>
+			Title:<br>
+			<input type='text' name='title' size='30' required><br>
+			Author:<br>
+			<input type='text' name='author' size='30' required><br>
+			Post:<br>
+        <textarea id='text' name='text'></textarea>
+		<div>
+		<input type='submit' value='Send'>
+		</div>
+	   </form>
+	   </div>
+	<script>tinymce.init({ selector:'textarea' });</script>
+	";
+	checkUploaded( $uploaded, $str , $correct , "yes" , "Blog post created" , $correct, false );
+	?>
+
+	<?php
 }
 else if ( isset( $req[ 'agenda' ] ) )
 {
-	checkUploaded( $uploaded, "TODO: Agenda Upload form needed" , $correct , "yes" , "Agenda uploaded" , $correct );
+	$str = "
+	   <form action='upload.php'
+			enctype='multipart/form-data' method='post'>
+			<p>
+			Title:<br>
+			<input type='text' name='title' size='30' required>
+			<input type='hidden' name='token' value='${token}'>
+			</p>
+			<p>
+			Please specify a file:<br>
+			<input type='file' name='file' required>
+			</p>
+			<div>
+			<input type='submit' value='Send'>
+			</div>
+			</form>
+		   </div>
+	";
+	checkUploaded( $uploaded, $str , $correct , "yes" , "Agenda uploaded" , $correct , false);
 }
 else if ( isset( $req[ 'roster' ] ) )
 {
@@ -54,5 +128,5 @@ else if ( isset( $req[ 'roster' ] ) )
 }
 else
 {
-	echo "TODO: Entrance page needed";
+
 }
